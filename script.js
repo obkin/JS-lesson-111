8cab116372d853976083316d597880eb59625370
obkin <kapellwork@gmail.com>
2022-10-27 11:23:26 +0300

    practice

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 "use strict";
 
+/*
+
 function *generator() {
     yield 'S';
     yield 'c';
@@ -12,3 +14,29 @@ function *generator() {
 const str = generator();
 
 console.log(str.next().done);
+
+*/
+
+// ------------------------------------------------
+
+function* count(n) {
+    for (let i = 0; i < n; i++) {
+        yield i;
+    }
+}
+
+const counter = count(7);
+
+console.log(counter.next().value);
+console.log(counter.next().value);
+console.log(counter.next().value);
+
+// ------------------------------------------------
+
+function some(n) {
+    for (let i = 0; i < n; i++) {
+        console.log(i);
+    }
+}
+
+some(7);
